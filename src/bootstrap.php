3a198034a3f7cdcf013fdf7da47c658be3df8082
Martin René Sørensen <git@sicet7.com>
2021-05-12 11:37:47 +0200

    Fixed issues.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -13,10 +13,10 @@ require_once $file;
 
 $client = new Client('mongodb://FranDineReadWrite:ReadWritePassword@mongo', [], ['typeMap' => DocumentManager::CLIENT_TYPEMAP]);
 $config = new Configuration();
-$config->setProxyDir(__DIR__ . '/Proxies');
-$config->setProxyNamespace('Proxies');
-$config->setHydratorDir(__DIR__ . '/Hydrators');
-$config->setHydratorNamespace('Hydrators');
+$config->setProxyDir(__DIR__ . '/app/Proxies');
+$config->setProxyNamespace('App\\Proxies');
+$config->setHydratorDir(__DIR__ . '/app/Hydrators');
+$config->setHydratorNamespace('App\\Hydrators');
 $config->setDefaultDB('FranDine');
 $config->setDocumentNamespaces(['App\\Documents']);
 $config->setMetadataDriverImpl(AnnotationDriver::create(__DIR__ . '/app/Documents'));
