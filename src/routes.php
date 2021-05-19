7dcd1d4605327bea2e09c67ca6da8e3698f256f0
Martin René Sørensen <git@sicet7.com>
2021-05-19 14:37:45 +0200

    Added service lib to orders api.

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,8 @@
  * @OA\Info(title="OrdersAPI", version="1.0.0")
  */
 
+use MMSM\Lib\AuthorizationMiddleware;
+use Slim\Middleware\BodyParsingMiddleware;
 use Slim\Routing\RouteCollectorProxy;
 use App\Actions\Create;
 use App\Actions\Read;
@@ -13,24 +15,23 @@ use App\Actions\ReadUser;
 use App\Actions\Update;
 use App\Actions\Delivered;
 use App\Actions\Delete;
-use App\Actions\SwaggerJson;
-use App\Middlewares\JsonBodyParserMiddleware;
-use OpenApi\Annotations as OA;
 
 /** @var \Slim\App $app */
+/** @var \Psr\Container\ContainerInterface $container */
 
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
-$app->group('/api', function (RouteCollectorProxy $group) {
-    $group->post('/orders', Create::class)->add(JsonBodyParserMiddleware::class);
+$authMiddleware = $container->get(AuthorizationMiddleware::class);
+$bodyMiddleware = $container->get(BodyParsingMiddleware::class);
+
+$app->group('/api/v1', function (RouteCollectorProxy $group) use ($bodyMiddleware) {
+    $group->post('/orders', Create::class)->add($bodyMiddleware);
     $group->get('/orders/{orderId}', Read::class);
     $group->get('/orders/{locationId}/last/{n}', ReadLast::class);
     $group->get('/orders/location/{locationId}/{sortBy}/{page}/{size}', ReadLocation::class);
     $group->get('/orders/user/{userId}/all', ReadUser::class);
-    $group->patch('/orders/{orderId}', Update::class)->add(JsonBodyParserMiddleware::class);
-    $group->patch('/orders/delivered/{orderId}', Delivered::class)->add(JsonBodyParserMiddleware::class);
+    $group->patch('/orders/{orderId}', Update::class)->add($bodyMiddleware);
+    $group->patch('/orders/delivered/{orderId}', Delivered::class)->add($bodyMiddleware);
     $group->delete('/orders/{orderId}', Delete::class);
-});
-
-$app->get('/swagger.json', SwaggerJson::class);
+})->add($authMiddleware);
