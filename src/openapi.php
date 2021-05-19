d5f227d83628fff7af996df9a8f16935c3846435
Martin René Sørensen <git@sicet7.com>
2021-05-19 12:57:15 +0200

    Fixed openapi generation to not include vendor folder.

--- src/openapi.php
+++ src/openapi.php
@@ -1,4 +1,8 @@
 <?php
 require_once 'vendor/autoload.php';
-$openapi = \OpenApi\scan(__DIR__ . '/app');
+$openapi = \OpenApi\scan(__DIR__, [
+    'exclude' => [
+        __DIR__ . '/vendor'
+    ]
+]);
 echo $openapi->toJson();
