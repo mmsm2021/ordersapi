90a57a3ecf1c2c9c302d933c134f29c77e7d063f
Martin René Sørensen <git@sicet7.com>
2021-05-20 13:59:52 +0200

    Updated ReadUser.

--- src/app/Actions/ReadUser.php
+++ src/app/Actions/ReadUser.php
@@ -4,69 +4,61 @@ namespace App\Actions;
 
 use App\Documents\Order;
 use Doctrine\ODM\MongoDB\DocumentManager;
-use Psr\Http\Message\ResponseInterface as Response;
-use Slim\Psr7\Factory\ResponseFactory;
+use MMSM\Lib\Factories\JsonResponseFactory;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Exception\HttpException;
 use Throwable;
 
 class ReadUser
 {
-    /** Document manager used for persisting Document */
-    private $documentManager;
-    /** Factory for HTTP response */
-    private $responseFactory;
+    /**
+     * Document manager used for persisting Documents
+     * @var DocumentManager
+     */
+    private DocumentManager $documentManager;
 
+    /**
+     * Factory for JSON HTTP response
+     * @var JsonResponseFactory
+     */
+    private JsonResponseFactory $responseFactory;
+
+    /**
+     * ReadUser constructor.
+     * @param DocumentManager $documentManager
+     * @param JsonResponseFactory $responseFactory
+     */
     public function __construct(
         DocumentManager $documentManager,
-        ResponseFactory $responseFactory
+        JsonResponseFactory $responseFactory
     ) {
         $this->documentManager = $documentManager;
         $this->responseFactory = $responseFactory;
     }
 
-    public function __invoke(Response $response, $userId)
+    /**
+     * @param $userId
+     * @return ResponseInterface
+     */
+    public function __invoke($userId): ResponseInterface
     {
         try {
-            $orders = $this->documentManager->getRepository(Order::class)->findBy(['customer' => $userId]);
+            $orders = $this->documentManager->getRepository(Order::class)->findBy([
+                'customer' => $userId
+            ]);
             $sendBack = [];
             foreach ($orders as $order) {
                 $sendBack[] = $order->toArray();
             }
-            $response->getBody()->write(json_encode(['orders' => $sendBack], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
-            return $response;
+            return $this->responseFactory->create(200, ['orders' => $sendBack]);
         } catch (Throwable $e) {
-            $response = $this->responseFactory->createResponse(400);
-            $response->getBody()->write($e->getMessage());
-            return $response;
-        }
-    }
-
-    function ordersArray($orders)
-    {
-        $ordersArray = [];
-
-        foreach ($orders as $order) {
-            $itemsArray = [];
-            $items = $order->getPersistentItems()->getValues();
-            foreach ($items as $item) {
-                $itemsArray[] = [
-                    'id' => $item->getId(),
-                    'name' => $item->getName(),
-                    'cost' => $item->getCost()
-                ];
+            if ($e instanceof HttpException) {
+                throw $e;
             }
-
-            $ordersArray[] = [
-                'orderId' => $order->getOrderID(),
-                'location'  => $order->getLocation(),
-                'locationId'  => $order->getLocationId(),
-                'server'  => $order->getServer(),
-                'customer'  => $order->getCustomer(),
-                'items'  => $itemsArray,
-                'discount'  => $order->getDiscount(),
-                'total'  => $order->getTotal(),
-                'orderDate'  => $order->getOrderDate()
-            ];
+            return $this->responseFactory->create(400, [
+                'error' => true,
+                'message' => $e->getMessage(),
+            ]);
         }
-        return $ordersArray;
     }
 }
