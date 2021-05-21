d4baefedeae0e759cf03db0e086ce33277365efa
NINLeviathan <stig_r@yahoo.com>
2021-05-21 08:05:33 +0200

    Clean up and comments

--- src/app/Actions/ReadLocation.php
+++ src/app/Actions/ReadLocation.php
@@ -4,8 +4,7 @@ namespace App\Actions;
 
 use App\Documents\Order;
 use Doctrine\ODM\MongoDB\DocumentManager;
-use Psr\Http\Message\ResponseInterface as Response;
-use Slim\Psr7\Factory\ResponseFactory;
+use MMSM\Lib\Factories\JsonResponseFactory;
 use Throwable;
 
 class ReadLocation
@@ -17,19 +16,32 @@ class ReadLocation
     private $documentManager;
 
     /**
-     * Factory for HTTP response
+     * Factory for JSON HTTP response
+     * @var JsonResponseFactory
      */
-    private $responseFactory;
+    private JsonResponseFactory $responseFactory;
 
+    /**
+     * ReadLocation constructor.
+     * @param DocumentManager $documentManager
+     * @param JsonResponseFactory $responseFactory
+     */
     public function __construct(
         DocumentManager $documentManager,
-        ResponseFactory $responseFactory
+        JsonResponseFactory $responseFactory
     ) {
         $this->documentManager = $documentManager;
         $this->responseFactory = $responseFactory;
     }
 
-    public function __invoke(Response $response, $locationId, $sortBy, $page, $size)
+    /**
+     * @param int $locationId
+     * @param string $sortBy
+     * @param int $page
+     * @param int $size
+     * @return ResponseInterface
+     */
+    public function __invoke($locationId, $sortBy, $page, $size)
     {
         try {
             $page = $page - 1;
@@ -38,12 +50,12 @@ class ReadLocation
             foreach ($orders as $order) {
                 $sendBack[] = $order->toArray();
             }
-            $response->getBody()->write(json_encode(['orders' => $sendBack], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
-            return $response;
+            return $this->responseFactory->create(200, ['orders' => $sendBack]);
         } catch (Throwable $e) {
-            $response = $this->responseFactory->createResponse(400);
-            $response->getBody()->write($e->getMessage());
-            return $response;
+            return $this->responseFactory->create(400, [
+                'error' => true,
+                'message' => $e->getMessage(),
+            ]);
         }
     }
 }
